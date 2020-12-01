8dcb67700695e45d5a3283e2b87d5a27dc82b369
tctsigemura <sigemura@tokuyama.kosen-ac.jp>
2020-12-01 09:14:25 +0900

    初期状態

---
+++ src/Ex9.java
@@ -0,0 +1,38 @@
+import java.awt.EventQueue;
+import javax.swing.JFrame;
+
+public class Ex9 {
+	private JFrame frame;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Ex9 window = new Ex9();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Ex9() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
